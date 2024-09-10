adc1e4cccffeb5c28ae16ad01841f5e1c68734fa
Hubol <hubol.gordon@gmail.com>
2024-09-09 22:28:53 -0500

    Getting somewhere

--- src/features/TextureRepository.hx
+++ src/features/TextureRepository.hx
@@ -16,6 +16,22 @@ typedef TextureDirectory =
     updatedAt: Float,
 }
 
+class FooPath {
+    public static function resolve(parent:String, child:String) {
+        return normalize(Path.resolve(parent, child));
+    }
+
+    public static function relative(parent:String, child:String) {
+        return normalize(Path.relative(parent, child));
+    }
+
+    static final windowsPathSeparatorRegExp = ~/\\/g;
+
+    public static function normalize(path:String) {
+        return windowsPathSeparatorRegExp.replace(path, '/');
+    }
+}
+
 class TextureRepository {
     var isDestroyed = false;
     final root:String;
@@ -26,7 +42,7 @@ class TextureRepository {
     final texturePathsToLoad = new Set<String>();
 
     public function new(path:String) {
-        this.root = normalizeSeparators(path);
+        this.root = FooPath.normalize(path);
         this.directory = createTextureDirectory();
         populateDirectoryFromWalker();
         this.watcher = Chokidar.watch([ this.root ])
@@ -44,7 +60,8 @@ class TextureRepository {
             if (existingTexture != null) {
                 existingTexture.dispose();
             }
-            final path = Path.resolve(this.root, texturePath);
+            final path = FooPath.resolve(this.root, texturePath);
+            trace(path);
             final texture = Texture.fromFile(path);
             textures.set(texturePath, texture);
         }
@@ -52,37 +69,13 @@ class TextureRepository {
         this.texturePathsToLoad.clear();
     }
 
-    function disposeDirectory(dir:TextureDirectory) {
-        final textureKeysToRemove: Array<String> = [];
-
-        for (texturePath in dir.texturePaths) {
-            final texture = this.getTexture(texturePath);
-            if (texture != null) {
-                texture.dispose();
-                textureKeysToRemove.push(texturePath);
-            }
-        }
-
-        for (keys in textureKeysToRemove)
-            this.textures.remove(keys);
-
-        for (directory in dir.directories)
-            this.disposeDirectory(directory);
-    }
-
     function populateDirectoryFromWalker() {
         new Walker(this.root)
         .on('data', item -> this.upsert(item.path, item.stats));
     }
 
-    static final windowsPathSeparatorRegExp = ~/\\/g;
-
-    static function normalizeSeparators(path:String) {
-        return windowsPathSeparatorRegExp.replace(path, '/');
-    }
-
     function normalize(path:String) {
-        return normalizeSeparators(Path.relative(this.root, normalizeSeparators(path)));
+        return FooPath.relative(this.root, path);
     }
 
     static function createTextureDirectory(): TextureDirectory {
@@ -211,6 +204,12 @@ class TextureRepository {
         this.delete(path);
     }
 
+    public function getDirectory(path:String) {
+        if (path == '')
+            return this.directory;
+        return this.directories.get(path);
+    }
+
     public function getTexture(path:String) {
         // TODO what happens when it doesn't exist?!
         return this.textures.get(path);
@@ -225,161 +224,56 @@ class TextureRepository {
             texture.dispose();
         this.isDestroyed = true;
     }
+}
 
-    // watchers[i] = Chokidar.watch(paths[i], {depth: OGMO.project.directoryDepth })
-	// 			.on('add', (path:String, stats:Stats) ->
-	// 			{
-	// 				if (path != paths[i] && items[i].children != null) recursiveAdd(path, stats, items[i]);
-	// 			})
-	// 			.on('addDir', (path:String, stats:Stats) ->
-	// 			{
-	// 				if (path != paths[i] && items[i].children != null) recursiveAdd(path, stats, items[i]);
-	// 			})
-	// 			.on('change', (path:String, stats:Stats) ->
-	// 			{
-	// 				// TODO: use this event to notify user of opened level changes? - austin
-	// 			})
-	// 			.on('unlink', (path:String) ->
-	// 			{
-	// 				if (path != paths[i] && items[i].children != null) recursiveRemove(path, items[i]);
-	// 			})
-	// 			.on('unlinkDir', (path:String) ->
-	// 			{
-	// 				if (path != paths[i] && items[i].children != null) recursiveRemove(path, items[i]);
-	// 			});
-
-    // files = { name: "root", parent: null, textures: [], subdirs: [] };
-
-	// 	function recursiveAdd(item:Item, parent:Files):Bool
-	// 	{
-	// 		if (OGMO.project == null) return false;
-	// 		var dirname = Path.dirname(item.path);
-	// 		if (dirname == Path.join(parent.dirname, parent.name) || parent.name == 'root' && dirname == parent.dirname)
-	// 		{
-	// 			// add to parent
-	// 			if (item.stats.isDirectory())
-	// 			{
-	// 				var obj = { name: Path.basename(item.path), dirname: dirname, parent: parent, textures: [], subdirs: [] };
-	// 				parent.subdirs.push(obj);
-	// 			}
-	// 			else if (item.stats.isFile())
-	// 			{
-	// 				parent.textures.push(haxe.io.Path.normalize(item.path));
-	// 			}
-	// 			return true;
-	// 		}
-	// 		else
-	// 		{
-	// 			var found = false;
-	// 			var i = 0;
-	// 			while (i < parent.subdirs.length && !found)
-	// 			{
-	// 				found = recursiveAdd(item, parent.subdirs[i]);
-	// 				i++;
-	// 			}
-	// 			return found;
-	// 		}
-	// 		return false;
-	// 	}
-
-    // var path = Path.join(Path.dirname(project.path), folder);
-	// 	files.dirname = path;
-	// 	if (FileSystem.exists(path))
-	// 	{
-	// 		walker = new Walker(path)
-	// 		.on("data", (item:Item) -> { if(item.path != path) recursiveAdd(item, files); })
-	// 		.on("end", () -> {
-	// 			// remove sequences
-	// 			if (!includeImageSequence)
-	// 			{
-	// 				function removeSequence (obj:Files)
-	// 				{
-	// 					// remove sequence
-	// 					obj.textures.sort(function(a, b)
-	// 					{
-	// 						if(a < b) return -1;
-	// 						if(a > b) return 1;
-	// 						return 0;
-	// 					});
-
-	// 					var newList:Array<String> = [];
-	// 					var lastName = "";
-	// 					for (texture in obj.textures)
-	// 					{
-	// 						// get next name
-	// 						var nextName = Path.basename(texture);
-	// 						// TODO - willl have to double check this
-	// 						nextName = '.' + nextName.split(".").pop();
-							
-	// 						// remove numbers
-	// 						var lastNumber = nextName.length - 1;
-	// 						while (lastNumber >= 0 && !nextName.charAt(lastNumber).parseInt().isNaN()) lastNumber --;
-	// 						nextName = nextName.substr(0, lastNumber + 1);
-
-	// 						// check if the last name was the same
-	// 						if (lastName == "" || lastName != nextName)
-	// 						{
-	// 							lastName = nextName;
-	// 							newList.push(texture);
-	// 						}
-	// 					}
-
-	// 					obj.textures = newList;
-
-	// 					// do the same on subdirectories
-	// 					for	(subdir in obj.subdirs) removeSequence(subdir);
-	// 				}
-
-	// 				removeSequence(files);
-	// 			}
-
-	// 			// load textures
-	// 			function loadTextures (obj:Files)
-	// 			{
-	// 				var textures = [];
-	// 				for (texture in obj.textures)
-	// 				{
-	// 					var ext = Path.extname(texture);
-	// 					if (ext != ".png" && ext != ".jpeg" && ext != ".jpg" && ext != ".bmp")
-	// 						continue;
-							
-	// 					var tex = Texture.fromFile(texture);
-	// 					if (tex != null)
-	// 					{
-	// 						tex.path = Path.relative(Path.dirname(project.path), texture);
-	// 						this.textures.push(tex);
-	// 						textures.push(tex);
-	// 					}
-	// 				}
-
-	// 				obj.textures = textures;
-
-	// 				for (subdir in obj.subdirs) loadTextures(subdir);
-	// 			}
-
-	// 			loadTextures(files);
-	// 			if (doRefresh != null) doRefresh();
-	// 		});
-	// 	}
+typedef TextureRepositoryPage = {
+    path:String,
+    exists:Bool,
+    subdirectoryNames:Array<String>,
+    texturePaths:Array<String>,
 }
 
 class TextureRepositoryPager {
     final repository:TextureRepository;
 
     var path = '';
-    var lastUpdatedAt = -1;
-    final subdirectoryNames = new Array<String>();
-    final texturePaths = new Array<String>();
+    var lastUpdatedAt:Float = -1;
 
     public function new(repository:TextureRepository) {
         this.repository = repository;
     }
 
-    public function update() {
+    public function update(): TextureRepositoryPage {
+        final directory = this.repository.getDirectory(this.path);
+        final subdirectoryNames = directory == null ? [] : [for (name in directory.directories.keys()) name];
+        final texturePaths = directory == null ? [] : [for (path in directory.texturePaths) path];
+        this.lastUpdatedAt = directory == null ? -1 : directory.updatedAt;
 
+        return {
+            path: this.path,
+            exists: directory != null,
+            subdirectoryNames: subdirectoryNames,
+            texturePaths: texturePaths,
+        }
     }
 
-    public function isOutdated():Bool {
-        return true;
+    public function into(subdirectoryName:String) {
+        this.path = Path.resolve(this.path, subdirectoryName);
+        this.lastUpdatedAt = -1;
     }
+
+    public function isOutdated():IsOutdatedResult {
+        final directory = this.repository.getDirectory(this.path);
+        if (directory == null)
+            return IsOutdatedResult.IsDeleted;
+        if (directory.updatedAt != this.lastUpdatedAt)
+            return IsOutdatedResult.HasChanges;
+        return IsOutdatedResult.UpToDate;
+    }
+}
+
+enum IsOutdatedResult {
+    UpToDate;
+    HasChanges;
+    IsDeleted;
 }
