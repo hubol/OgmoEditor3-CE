b928517361230fbea3d9c461ab36ba8b6b09eca8
Hubol <hubol.gordon@gmail.com>
2024-10-01 18:30:15 -0500

    Add @jaames/iro, provide haxe typings

---
+++ src/util/IroJs.hx
@@ -0,0 +1,37 @@
+package util;
+
+import js.html.Element;
+
+typedef IroColorPickerLayoutItemOptions = {
+	?id:String,
+	?sliderType:String,
+}
+
+typedef IroColorPickerLayoutItem = {
+	component:Dynamic,
+	?options:IroColorPickerLayoutItemOptions,
+}
+
+typedef IroColorPickerOptions = {
+	width:Int,
+	color:String,
+	borderWidth:Int,
+	borderColor:String,
+	layout:Array<IroColorPickerLayoutItem>,
+}
+
+@:jsRequire("@jaames/iro", "default.ColorPicker")
+extern class IroColorPicker
+{
+	@:overload(function(el:String, ?options:IroColorPickerOptions):Void {})
+	public function new(el:Element, ?options:IroColorPickerOptions):Void;
+
+	public function on(event:String, listener:String->Void):Void;
+}
+
+@:jsRequire("@jaames/iro", "default.ui")
+extern class IroComponents
+{
+	public static final Box:Dynamic;
+	public static final Slider:Dynamic;
+}
