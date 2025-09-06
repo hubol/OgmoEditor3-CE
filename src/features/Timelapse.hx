de56a7f73a70173f220b8329e4e0ff23b88bcd5e
hubol <hubol.gordon@gmail.com>
2025-09-06 18:31:02 -0500

    Stupid hacks, but less code!

--- src/features/Timelapse.hx
+++ src/features/Timelapse.hx
@@ -14,7 +14,6 @@ typedef ScreenshotData = {
 enum State {
     NotReady;
     Delay;
-    WaitForLevelsPanelItems;
     TakeAndSubmitScreenshots;
 }
 
@@ -29,7 +28,11 @@ class Timelapse {
 
     public function initialize() {
         TimelapseClient.getTask()
-            .then(task -> Ogmo.startPage.onOpenProject(task.ogmoProjectPath))
+            .then(task -> {
+                this.levelPaths = task.levelPaths;
+                this.remainingScreenshotsCount = this.levelPaths.length;
+                return Ogmo.startPage.onOpenProject(task.ogmoProjectPath);
+            })
             .then((cast (() -> this.state = Delay): Dynamic));
     }
 
@@ -40,46 +43,28 @@ class Timelapse {
 
     public function loop() {
         if (this.state == Delay) {
-            if (this.delayedCount++ >= 60) {
-                this.state = WaitForLevelsPanelItems;
-            }
-        }
-        else if (this.state == WaitForLevelsPanelItems) {
-            final levelPaths = findLevelPaths(Ogmo.editor.levelsPanel.items, []);
-            if (Ogmo.editor.levelsPanel.items.length > 0 && levelPaths.length > 0) {
-                this.levelPaths = levelPaths;
-                this.remainingScreenshotsCount = this.levelPaths.length;
+            if (this.delayedCount++ >= 10) {
                 this.state = TakeAndSubmitScreenshots;
             }
         }
         else if (this.state == TakeAndSubmitScreenshots) {
+            final levelDirectoryPath = OGMO.project.getAbsoluteLevelDirectories()[0];
+
             if (this.currentLevelPath == null && this.levelPaths.length > 0) {
-                final levelDirectoryPath = OGMO.project.getAbsoluteLevelDirectories()[0];
-                final levelPath = this.levelPaths.pop();
-                this.currentLevelPath = levelPath;
-                Ogmo.editor.levelManager.open(levelPath);
+                this.delayedCount = 0;
+                this.currentLevelPath = this.levelPaths.pop();
+                Ogmo.editor.levelManager.open(this.currentLevelPath);
+            }
+            else if (this.delayedCount++ == 4) {
                 Ogmo.editor.saveLevelAsImage((png) -> {
-                    this.currentLevelPath = null;
-                    TimelapseClient.submitScreenshot(levelPath.substring(levelDirectoryPath.length), png)
+                    TimelapseClient.submitScreenshot(this.currentLevelPath.substring(levelDirectoryPath.length), png)
                         .then((cast (() -> this.remainingScreenshotsCount -= 1): Dynamic));
+                    this.currentLevelPath = null;
                 });
             }
             else if (this.remainingScreenshotsCount <= 0) {
                 Browser.window.location.reload();
             }
         }
-    }
-
-    static function findLevelPaths(items:Array<PanelItem>, levelPaths:Array<String>) {
-        for (item in items) {
-            if (item.children != null) {
-                findLevelPaths(item.children, levelPaths);
-            }
-            else if (Path.extname(item.path) == ".json") {
-                levelPaths.push(item.path);
-            }
-        }
-
-        return levelPaths;
     }
 }
