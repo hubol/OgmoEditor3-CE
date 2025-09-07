079e1049c66f079ce557da3d9d8f83981ed2c504
hubol <hubol.gordon@gmail.com>
2025-09-06 20:13:23 -0500

    Try to detect when decals aren't rendering properly

--- src/features/Timelapse.hx
+++ src/features/Timelapse.hx
@@ -1,5 +1,6 @@
 package features;
 
+import modules.decals.DecalLayer;
 import js.html.Console;
 import js.Browser;
 import js.node.Path;
@@ -31,17 +32,21 @@ class Timelapse {
     public function initialize() {
         TimelapseClient.getTask()
             .then(task -> {
-                this.levelPaths = task.levelPaths;
-                this.remainingScreenshotsCount = this.levelPaths.length;
+                this.levelPaths = task.levelPaths.copy();
+                this.levelPathsToScreenshot = task.levelPaths.copy();
                 return Ogmo.startPage.onOpenProject(task.ogmoProjectPath);
             })
             .then((cast (() -> this.state = Delay): Dynamic));
     }
 
     var levelPaths: Array<String>;
+    var levelPathsToScreenshot: Array<String>;
     var currentLevelPath: Null<String>;
     var remainingScreenshotsCount: Int;
     var delayedCount = 0;
+
+    var visitAllAttempts = 0;
+    var mustVisitAllAgain = false;
 
     public function loop() {
         if (this.state == Delay) {
@@ -52,36 +57,67 @@ class Timelapse {
         }
         else if (this.state == VisitAll) {
             if (this.delayedCount < this.levelPaths.length) {
-                Ogmo.editor.levelManager.open(this.levelPaths[this.delayedCount]);
+                final path = this.levelPaths[this.delayedCount];
+                Ogmo.editor.levelManager.open(path);
+                Ogmo.editor.isDirty = true;
                 this.delayedCount += 1;
+
+                if (doAnyDecalLayersHaveBrokenTextures()) {
+                    if (visitAllAttempts > 2) {
+                        Console.log(path, "appears to be poisoned!");
+                        levelPathsToScreenshot.remove(path);
+                    }
+                    else {
+                        mustVisitAllAgain = true;
+                    }
+                }
             }
             else {
-                this.state = TakeAndSubmitScreenshots;
+                if (!mustVisitAllAgain) {
+                    this.state = TakeAndSubmitScreenshots;
+                    this.remainingScreenshotsCount = this.levelPathsToScreenshot.length;
+                }
+                else {
+                    this.delayedCount = 0;
+                    this.mustVisitAllAgain = false;
+                    this.visitAllAttempts += 1;
+                }
             }
         }
         else if (this.state == TakeAndSubmitScreenshots) {
             final levelDirectoryPath = OGMO.project.getAbsoluteLevelDirectories()[0];
-            
-            if (this.currentLevelPath == null && this.levelPaths.length > 0) {
-                this.delayedCount = 0;
-                this.currentLevelPath = this.levelPaths.pop();
+
+            if (this.currentLevelPath == null && this.levelPathsToScreenshot.length > 0) {
+                this.currentLevelPath = this.levelPathsToScreenshot.pop();
                 Ogmo.editor.levelManager.open(this.currentLevelPath);
-            }
-            else if (this.delayedCount++ == 5) {
-                final struggle = (_:String) -> Ogmo.editor.saveLevelAsImage((png) -> {
+                Ogmo.editor.saveLevelAsImage((png) -> {
                     TimelapseClient.submitScreenshot(this.currentLevelPath.substring(levelDirectoryPath.length), png)
                         .then((cast (() -> this.remainingScreenshotsCount -= 1): Dynamic));
                     this.currentLevelPath = null;
                 });
-
-                Ogmo.editor.saveLevelAsImage(struggle);
             }
             else if (this.remainingScreenshotsCount <= 0) {
-                Browser.window.location.reload();
-            }
-            else {
-                Ogmo.editor.isDirty = true;
+                complete();
             }
         }
+    }
+
+    private static function complete() {
+        Browser.window.location.reload();
+    }
+
+    private static function doAnyDecalLayersHaveBrokenTextures() {
+        for (layer in Ogmo.editor.level.layers) {
+            if (layer.isOfType(DecalLayer)) {
+                final decalLayer = (cast layer: DecalLayer);
+                for (decal in decalLayer.decals) {
+                    if (decal.texture.getTexture() == BrokenTexture.instance) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
